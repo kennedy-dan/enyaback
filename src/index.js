72a8ecebbde16a55dc0966bd2a8d90caa33c9ce3
kennedy-dan <61075930+kennedy-dan@users.noreply.github.com>
2021-01-23 04:29:42 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -4,25 +4,25 @@ const fs = require('fs');
 const app = express()
 const fetch = require("node-fetch");
 app.use(express.json())
-const csvF = async(req, res) => {
-   
-        const {base, money} = req.query
 
-    try{
-        await fetch(`https://api.exchangeratesapi.io/latest?base=${ base }&symbols=${ money }`)
-        .then(res => res.json())
-        .then(results => res.status(200).send({result:results}));
-    }catch (error){
-        return res.status(400).send({
-            error:error.message,
-            message: 'request unsuccessful'
-        })
-    }
-  
-    
+
+app.get('/api/rates',async(req, res) => {
+   
+    const {base, money} = req.query
+
+try{
+    await fetch(`https://api.exchangeratesapi.io/latest?base=${ base }&symbols=${ money }`)
+    .then(res => res.json())
+    .then(results => res.status(200).send({result:results}));
+}catch (error){
+    return res.status(400).send({
+        error:error.message,
+        message: 'request unsuccessful'
+    })
 }
 
-app.get('/api/rates', csvF
+
+}
 )
 
 app.listen(process.env.PORT || 3000, () => {
