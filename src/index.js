dff01d1cd902acbacc3e83fd0b9a4d47654cd3e6
kennedy-dan <61075930+kennedy-dan@users.noreply.github.com>
2021-01-23 04:40:46 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -2,28 +2,33 @@ const express = require('express')
 const csv = require('csv-parser');
 const fs = require('fs');
 const app = express()
+const axios = require('axios')
 const fetch = require("node-fetch");
 app.use(express.json())
 
 
 app.get('/api/rates',async(req, res) => {
    
-    const {base, money} = req.query
+    const {base, currency} = req.query
+    try {
+        const results = await axios({
+            url: encodeURI(`https://api.exchangeratesapi.io/latest?base=${ base }&symbols=${ currency }`),
+            method: 'get',
+        })
 
-try{
-    await fetch(`https://api.exchangeratesapi.io/latest?base=${ base }&symbols=${ money }`)
-    .then(res => res.json())
-    .then(results => res.status(200).send({result:results}));
-}catch (error){
-    return res.status(400).send({
-        error:error.message,
-        message: 'request unsuccessful'
-    })
-}
-
-
-}
-)
+        return res.status(200).send({
+            status_code: 200,
+            results: results.data 
+        });
+          
+    } catch (error) {
+        return res.status(400).send({
+            status_code: 400,
+            detail: error.message,
+            message: 'Invalid request'
+        })
+    }
+})
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`server is running `)
