f4a70420a51ba292af435006afb6903243b9e23d
kennedy-dan <61075930+kennedy-dan@users.noreply.github.com>
2021-01-23 03:25:17 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const csvF = async(req, res) => {
 app.get('/api/rates', csvF
 )
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`server is running on port ${3000}`)
 })
 
